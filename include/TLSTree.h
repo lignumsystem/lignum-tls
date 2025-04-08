e711af5d434b8a07fab69590cedafb543cf7f1f6
jariperttunen <jari.perttunen@icloud.com>
2025-04-08 08:52:54 +0300

    TLSBud to be used in lignum-tls. Add data members and functionality as needed.

--- include/TLSTree.h
+++ include/TLSTree.h
@@ -1,11 +1,12 @@
+///\file TLSTree.h
+///Tree segment and bud for lignum-tls project.
+///
+///Enhance implementations as required during  model development.
 #ifndef TLSTREE_H
 #define TLSTREE_H
 #include <Lignum.h>
 #include <Kite.h>
-///\file TLSTree.h
-///\brief Tree segment and bud for lignum-tls project.
-///
-///Enhance implementations as required during  model development.
+
 using namespace Lignum;
 namespace LignumTLS{
   
@@ -15,6 +16,14 @@ namespace LignumTLS{
   ///Currently HwTreeSegment with Kite leaf.
   class TLSSegment: public HwTreeSegment<TLSSegment,TLSBud,cxxadt::Kite>{
   public:
+    ///\brief Constructor, identical in HwTreeSegment
+    ///\param p Base point of the segment
+    ///\param d Direction of the segment
+    ///\param go Gravelius order
+    ///\param l Segment length
+    ///\param r Segment radius
+    ///\param rh Segment heartwood radius
+    ///\param tree Tree the segment belongs to
     TLSSegment(const Point& p,const PositionVector& d, const LGMdouble go,const METER l,
 	       const METER r,const METER rh,Tree<TLSSegment,TLSBud>* tree)
       :HwTreeSegment(p,d,go,l,r,rh,tree){}
@@ -26,8 +35,13 @@ namespace LignumTLS{
   ///Currently Bud with TLSSegment and TLSBud.
   class TLSBud: public  Bud<TLSSegment,TLSBud>{
   public:
-    TLSBud(const Point& p, const PositionVector& d, const LGMdouble omega, Tree<TLSSegment,TLSBud>* tree)
-      :Bud(p,d,omega,tree){}
+    ///\brief Constructor, identical in Bud
+    ///\param p  Position of the bud
+    ///\param d Direction of the bud
+    ///\param go Gravelius order
+    ///\param tree Tree the bud belongs to
+    TLSBud(const Point& p, const PositionVector& d, const LGMdouble go, Tree<TLSSegment,TLSBud>* tree)
+      :Bud(p,d,go,tree){}
   };
 }
 #endif
